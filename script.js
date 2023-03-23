32172dd4b573b9d3dd00f4c54f21947efc442eeb
Timo <tpripon99@gmail.com>
2023-03-23 21:28:36 +0200

    perete/acoperis

--- script.js
+++ script.js
@@ -11,4 +11,16 @@ function draw() {
   fill("#9bd1c9");
   ellipse(200, 600, 600, 300);
 
+// perete
+fill("#f7dc6f");
+strokeWeight(4);
+stroke("#d4ac0d");
+rect(150, 200, 300, 300);
+
+// acoperiș
+fill("#e74c3c");
+strokeWeight(4);
+stroke("#c0392b");
+triangle(150, 200, 450, 200, 300, 100);
+
 }
