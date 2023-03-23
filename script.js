98709b9db15388ef33e121620589f8acdecd4087
Timo <tpripon99@gmail.com>
2023-03-23 21:27:52 +0200

    dealuri

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function setup() {
-    createCanvas(400,400)
+  createCanvas(600, 600);
+  background("#87ceeb");
 }
 
-function draw () {
-background("#a0daa9")
-house();
-}
-
-function house () {
+function draw() {
+  // dealuri verzi
+  fill("#a8e6cf");
+  noStroke();
+  ellipse(500, 600, 800, 500);
+  fill("#9bd1c9");
+  ellipse(200, 600, 600, 300);
 
 }
